60c9b6cd4ea5f0b795d5d2d408296e97ac3d6775
dam-ari <bareket.damari@live.biu.ac.il>
2023-12-20 23:13:05 -0500

    🌐 Refactor hazardous item checking and add CORS headers

--- server/is-hazardous.js
+++ server/is-hazardous.js
@@ -11,21 +11,28 @@ export default async (req, res) => {
       "https://silver-box.npkn.net/hazardous-list"
     );
     const knownProblematicIngredients = response.data; // Adjust according to the actual response structure
-
     // Get the list of items to check from the request body
-    const itemsToCheck = req.body.items;
-    console.log(itemsToCheck);
+    const itemsToCheck = req.body?.items;
+    console.log(knownProblematicIngredients);
 
-    // Check each item and filter out hazardous ones
-    const hazardousItems = itemsToCheck.filter((item) =>
-      knownProblematicIngredients.includes(item)
-    );
+    if (itemsToCheck) {
+      // Check each item and filter out hazardous ones
+      const hazardousItems = itemsToCheck.filter((itemToCheck) =>
+        knownProblematicIngredients.some(
+          (knownItem) => knownItem.toLowerCase() === itemToCheck.toLowerCase()
+        )
+      );
 
-    // Send the result back
-    res.json({ hazardousItems });
+      res.headers["my-header"] = "hello";
+      res.headers["Access-Control-Allow-Origin"] = "*";
+      res.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS";
+      res.headers["Access-Control-Allow-Headers"] = "Content-Type";
+
+      // Send the result back
+      res.json({ hazardousItems });
+    }
   } catch (error) {
     console.error("Error:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
- 
